94eae3f00c2b9c1194f6a1edc8a9fb81d4ad2a96
waithawoo <waithawoocw@gmail.com>
2024-10-28 11:05:48 +0700

    fix(db) : check db value

--- src/db/SQLiteDatabase.js
+++ src/db/SQLiteDatabase.js
@@ -23,53 +23,56 @@ class SQLiteDatabase extends BaseDatabase {
     async createTable(tableName, columns) {
         const columnDefs = columns.join(", ");
         const sql = `CREATE TABLE IF NOT EXISTS ${tableName} (${columnDefs})`;
-
-        return new Promise((resolve, reject) => {
-            this.db.serialize(() => {
-                this.db.run(sql, (err) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve();
-                    }
+        if(this.db){
+            return new Promise((resolve, reject) => {
+                this.db.serialize(() => {
+                    this.db.run(sql, (err) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
                 });
             });
-        });
+        }
     }
 
     async insert(tableName, data) {
         const placeholders = Object.keys(data).map(() => '?').join(', ');
         const sql = `INSERT INTO ${tableName} (${Object.keys(data).join(', ')}) VALUES (${placeholders})`;
-
-        return new Promise((resolve, reject) => {
-            this.db.run(sql, Object.values(data), function (err) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(this.lastID);
-                }
+        if(this.db){
+            return new Promise((resolve, reject) => {
+                this.db.run(sql, Object.values(data), function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(this.lastID);
+                    }
+                });
             });
-        });
+        }
     }
 
     async query(tableName, conditions = {}) {
         let sql = `SELECT * FROM ${tableName}`;
         const conditionKeys = Object.keys(conditions);
-
-        if (conditionKeys.length > 0) {
-            const whereClauses = conditionKeys.map(key => `${key} = ?`).join(' AND ');
-            sql += ` WHERE ${whereClauses}`;
-        }
-
-        return new Promise((resolve, reject) => {
-            this.db.all(sql, Object.values(conditions), (err, rows) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows);
-                }
+        if(this.db){
+            if (conditionKeys.length > 0) {
+                const whereClauses = conditionKeys.map(key => `${key} = ?`).join(' AND ');
+                sql += ` WHERE ${whereClauses}`;
+            }
+    
+            return new Promise((resolve, reject) => {
+                this.db.all(sql, Object.values(conditions), (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(rows);
+                    }
+                });
             });
-        });
+        }
     }
 
     async close() {
